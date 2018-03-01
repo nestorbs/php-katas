a588305bfcc18d7bafa47a5a57b290a5da8d604a
Néstor Bethencourt <nestorbs@gmail.com>
2018-03-01 09:46:10 +0000

    green

--- WordWrap/src/Wrapper.php
+++ WordWrap/src/Wrapper.php
@@ -8,10 +8,18 @@ class Wrapper
     {
         $lines = '';
         $start = 0;
-        while (strlen(substr($word, $start)) > 0) {
-            $lines = $lines . substr($word, $start, $columns) . '\n';
+        while (strlen(substr($this->removeBeginningSpace($word), $start)) > 0) {
+            $lines = $lines . substr($this->removeBeginningSpace($word), $start, $columns) . '\n';
             $start = $start + $columns;
         }
         return $lines;
+    }
+
+    private function removeBeginningSpace($word)
+    {
+        if (strpos($word, ' ') === 0) {
+            return substr($word, 1);
+        }
+        return $word;
     }
 }
