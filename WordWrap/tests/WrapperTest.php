dae078ea1f451deb2cd0ea12137110dd64460c53
Néstor Bethencourt <nestorbs@gmail.com>
2018-03-01 09:48:20 +0000

    refactor: extract variable

--- WordWrap/tests/WrapperTest.php
+++ WordWrap/tests/WrapperTest.php
@@ -7,35 +7,34 @@ use nestorbs\Wrapper;
 
 class WrapperTest extends TestCase
 {
+    protected $wrapper;
+
+    protected function setUp()
+    {
+        $this->wrapper = new Wrapper();
+    }
+
     /** @test */
     public function should_return_full_word_when_word_length_is_shorter_than_column()
     {
-        $wrapper = new Wrapper();
-
-        $this->assertEquals('Car\n', $wrapper->wrap('Car', 6));
+        $this->assertEquals('Car\n', $this->wrapper->wrap('Car', 6));
     }
 
     /** @test */
     public function should_return_splitted_word_when_word_length_is_longer_than_column()
     {
-        $wrapper = new Wrapper();
-
-        $this->assertEquals('Ca\nr\n', $wrapper->wrap('Car', 2));
+        $this->assertEquals('Ca\nr\n', $this->wrapper->wrap('Car', 2));
     }
 
     /** @test */
     public function should_return_one_break_line_per_column_needed()
     {
-        $wrapper = new Wrapper();
-
-        $this->assertEquals('to\nbe\nor\n', $wrapper->wrap('tobeor', 2));
+        $this->assertEquals('to\nbe\nor\n', $this->wrapper->wrap('tobeor', 2));
     }
 
     /** @test */
     public function should_remove_space_at_the_beginning_of_the_line()
     {
-        $wrapper = new Wrapper();
-
-        $this->assertEquals('to\n', $wrapper->wrap(' to', 2));
+        $this->assertEquals('to\n', $this->wrapper->wrap(' to', 2));
     }
 }
